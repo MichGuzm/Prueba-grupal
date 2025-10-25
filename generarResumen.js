79a34c913a9f93c6a3850548c109fc8cb1e4b2a4
KRAT0Sss <justinecheverriaggtr@gmail.com>
2025-10-25 16:28:27 -0500

    generarResumen

---
+++ generarResumen.js
@@ -0,0 +1,21 @@
+let asistencia=[
+    {estudiante:"Juan Perez",lista:"A" },
+    {estudiante:"Juan Perez",lista:"F" }
+
+];
+
+revisarAsistencia=function(){
+    for(let i=0;i<asistencia.length;i++){
+        let presente=asistencia[i].lista
+        if(presente.lista==="A"){
+            let totalPresentes;
+            totalPresentes+=presente
+        } else if (presente.lista==="F"){
+            let totalAusentes;
+            totalAusentes
+        }
+    }
+}
+
+
+
